d40bb60c30da65c54dec807e32d265597a79b3c0
Karolis <k.stulgys@gmail.com>
2018-05-07 16:14:32 +1000

    added semantic-ui

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 import React from "react";
+import "semantic-ui-css/semantic.min.css";
+import { Button } from "semantic-ui-react";
 import { render } from "react-dom";
 import Hello from "./Hello";
+
+const ButtonExampleColored = () => (
+  <div>
+    <Button color="red">Red</Button>
+    <Button color="orange">Orange</Button>
+    <Button color="yellow">Yellow</Button>
+    <Button color="olive">Olive</Button>
+    <Button color="green">Green</Button>
+    <Button color="teal">Teal</Button>
+    <Button color="blue">Blue</Button>
+    <Button color="violet">Violet</Button>
+    <Button color="purple">Purple</Button>
+    <Button color="pink">Pink</Button>
+    <Button color="brown">Brown</Button>
+    <Button color="grey">Grey</Button>
+    <Button color="black">Black</Button>
+  </div>
+);
 
 const styles = {
   fontFamily: "sans-serif",
@@ -9,6 +29,7 @@ const styles = {
 
 const App = () => (
   <div style={styles}>
+    <ButtonExampleColored />
     <Hello name="CodeSandbox" />
     <h2>Start editing to see some magic happen {"\u2728"}</h2>
   </div>
