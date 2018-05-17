4f36e60e8cc1299cf7baeb52339a623490ae2a3e
Karolis <k.stulgys@gmail.com>
2018-05-17 16:52:34 +1000

    add redux-form to eventForm

--- src/features/event/EventForm/EventForm.js
+++ src/features/event/EventForm/EventForm.js
@@ -1,23 +1,25 @@
 import React, { Component } from "react";
-import { Segment, Button, Form } from "semantic-ui-react";
+import { Segment, Button, Form, Grid, Header } from "semantic-ui-react";
 import cuid from "cuid";
 import { connect } from "react-redux";
 import { createEvent, updateEvent } from "../eventActions";
+import { reduxForm, Field } from "redux-form";
+import TextInput from "../../../app/common/form/TextInput";
+import TextArea from "../../../app/common/form/TextArea";
+import SelectInput from "../../../app/common/form/SelectInput";
+const category = [
+  { key: "drinks", text: "Drinks", value: "drinks" },
+  { key: "dance", text: "Dance", value: "dance" },
+  { key: "coding", text: "Coding", value: "coding" }
+];
 
 const mapState = (state, ownProps) => {
-  let event = {
-    id: null,
-    title: "",
-    date: "",
-    city: "",
-    venue: "",
-    hostedBy: ""
-  };
+  let event = {};
   const eventId = ownProps.match.params.id;
   if (eventId && state.events.length > 0) {
     event = state.events.filter(event => event.id === eventId)[0];
   }
-  return { event };
+  return { initialValues: event };
 };
 
 const actions = {
@@ -26,25 +28,14 @@ const actions = {
 };
 
 class EventForm extends Component {
-  state = {
-    event: Object.assign({}, this.props.event)
-  };
-
-  handleInputChange = e => {
-    const newEvent = this.state.event;
-    newEvent[e.target.name] = e.target.value;
-    this.setState({ event: newEvent });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-
-    if (this.props.selectedEvent) {
-      this.props.updateEvent(this.state.event);
+  handleSubmit = values => {
+    if (this.props.initialValues.id) {
+      this.props.updateEvent(values);
       this.props.history.goBack();
     } else {
       const newEvent = {
-        ...this.state.event,
+        ...values,
+        hostedBy: "Bob",
         id: cuid(),
         hostPhotoUrl:
           "https://robohash.org/ipsumquoautem.jpg?size=100x100&set=set1"
@@ -55,72 +46,72 @@ class EventForm extends Component {
   };
 
   render() {
-    console.log("selectedE:", this.props.selectedEvent);
     return (
-      <Segment raised color="orange">
-        <Form>
-          <Form.Field>
-            <label>Event Title</label>
-            <input
-              onChange={this.handleInputChange}
-              placeholder="First Name"
-              name="title"
-              value={this.state.event.title}
-            />
-          </Form.Field>
-          <Form.Field>
-            <label>Event Date</label>
-            <input
-              onChange={this.handleInputChange}
-              placeholder="Event Date"
-              name="date"
-              type="date"
-              value={this.state.event.date}
-            />
-          </Form.Field>
-          <Form.Field>
-            <label>City</label>
-            <input
-              onChange={this.handleInputChange}
-              placeholder="City"
-              name="city"
-              value={this.state.event.city}
-            />
-          </Form.Field>
-          <Form.Field>
-            <label>Venue</label>
-            <input
-              onChange={this.handleInputChange}
-              placeholder="Venue"
-              name="venue"
-              value={this.state.event.venue}
-            />
-          </Form.Field>
-          <Form.Field>
-            <label>Hosted By</label>
-            <input
-              onChange={this.handleInputChange}
-              placeholder="Hosted By"
-              name="hostedBy"
-              value={this.state.event.hostedBy}
-            />
-          </Form.Field>
-          <Button
-            onClick={this.handleSubmit}
-            basic
-            content="Submit"
-            color="orange"
-          />
-          <Button
-            onClick={this.props.history.goBack}
-            basic
-            content="Cancel"
-            color="brown"
-          />
-        </Form>
-      </Segment>
+      <Grid>
+        <Grid.Column width={10}>
+          <Segment raised color="orange">
+            <Header sub content="Event Details" color="orange" />
+            <Form>
+              <Field
+                placeholder="Event title"
+                name="title"
+                component={TextInput}
+                type="text"
+              />
+              <Field
+                placeholder="Event category"
+                name="category"
+                component={SelectInput}
+                options={category}
+                type="text"
+              />
+              <Field
+                component={TextArea}
+                rows="3"
+                type="text"
+                placeholder="Event description"
+                name="description"
+              />
+              <Header sub content="Location Details" color="orange" />
+              <Field
+                component={TextInput}
+                type="text"
+                placeholder="Event Date"
+                name="date"
+              />
+              <Field
+                component={TextInput}
+                type="text"
+                placeholder="City"
+                name="city"
+              />
+              <Field
+                component={TextInput}
+                type="text"
+                placeholder="Venue"
+                name="venue"
+              />
+
+              <Button
+                onClick={this.props.handleSubmit(this.handleSubmit)}
+                basic
+                content="Submit"
+                color="orange"
+              />
+              <Button
+                onClick={this.props.history.goBack}
+                basic
+                content="Cancel"
+                color="brown"
+              />
+            </Form>
+          </Segment>
+        </Grid.Column>
+      </Grid>
     );
   }
 }
 
-export default connect(mapState, actions)(EventForm);
+export default connect(mapState, actions)(
+  reduxForm({ form: "eventForm", enableReinitialize: true })(EventForm)
+);
