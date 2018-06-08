ec7f9a68d45d065e52f982af8cdade599dbfb9c7
Karolis Stulgys <karolis.stulgys@ci.local>
2018-06-08 15:34:06 +1000

    fix eventAction fetchEvents

--- src/features/event/eventActions.js
+++ src/features/event/eventActions.js
@@ -3,45 +3,45 @@ import {
   CREATE_EVENT,
   UPDATE_EVENT,
   DELETE_EVENT
-} from "./eventConstants"
+} from "./eventConstants";
 import {
   asyncActionError,
   asyncActionFinish,
   asyncActionStart
-} from "../async/asyncActions"
+} from "../async/asyncActions";
 
-import { fetchSampleData } from "../../app/data/mockApi"
+import { fetchSampleData } from "../../app/data/mockApi";
 
 export const createEvent = event => ({
   type: CREATE_EVENT,
   payload: { event }
-})
+});
 
 export const updateEvent = event => ({
   type: UPDATE_EVENT,
   payload: { event }
-})
+});
 
 export const deleteEvent = eventId => ({
   type: DELETE_EVENT,
   payload: { eventId }
-})
+});
 
 export const fetchEvents = events => ({
   type: FETCH_EVENTS,
-  payload: events
-})
+  payload: { events }
+});
 
 export const loadEvents = () => {
   return async dispatch => {
     try {
-      dispatch(asyncActionStart())
-      let events = await fetchSampleData()
-      dispatch(fetchEvents(events))
-      dispatch(asyncActionFinish())
+      dispatch(asyncActionStart());
+      let events = await fetchSampleData();
+      dispatch(fetchEvents(events));
+      dispatch(asyncActionFinish());
     } catch (error) {
-      console.log(error)
-      dispatch(asyncActionError())
+      console.log(error);
+      dispatch(asyncActionError());
     }
-  }
-}
+  };
+};
